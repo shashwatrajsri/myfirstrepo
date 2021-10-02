b7301ed6ccfe7141e0bbf69ecda4598d1377dd41
shashwatrajsri <ss4521811@gmail.com>
2021-10-02 16:49:48 +0530

    functions

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var list = document.querySelector('ul')
+//function greet(name = 'john', age = 99){
+    //console.log(name,age)
+    //console.log('hey' + name + "your age is" age)
+    //console.log('hello')
 
-var arr = ['Go to gym' , 'cook oatmeal', 'eat']
 
-arr.push("complete assignment")
+//greet('Shashwat',21)
 
-for(var i=0;i<arr.length;i++){
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-    element.appendChild(textNode) 
-    list.appendChild(element)
+function add(a=0,b=0){
+    var sum=a+b;
+    return sum;
 }
+
+console.log(add(10,20))
