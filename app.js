126a4fd32017438f213c71307067ce85d9d575aa
shashwatrajsri <ss4521811@gmail.com>
2021-10-02 18:09:59 +0530

    todo list implementation

--- app.js
+++ app.js
@@ -16,16 +16,48 @@
     return sum;
 }
 
-console.log(add(10,20))*/
+console.log(add(10,20))
 
 var add = (a=0,b=0) => {
-    var sum = a + b
-    return sum
+    return a+b
 }
+
+var add = (a=0,b=0) => a+b
 
 console.log(add(10, 20))
 
+const greet = name => 'h1' + name
+console.log(greet('prabhakaran'))*/
 
+//var arr = ['go to the gym', 'eat food', 'clean house']
+/*for(var i=0;i<arr.length;i++){
+    console.log(arr[i])
+}*/
+
+//const greet = age => 'hello' + age
+//console.log(greet('Shashwat'))
+
+//var callbackfunction = (element, index)=>{
+    //console.log(element,index)
+//}
+
+//arr.forEach((element, index) => 
+    //console.log(element,index)
+//)
+
+var button= document.querySelector('button')
+var input= document.querySelector('input')
+var list= document.querySelector('ul')
+
+const callbackfunc = (event) => {
+    const inputValue = input.value
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
+
+button.addEventListener('click', callbackfunc)
 
 
 
