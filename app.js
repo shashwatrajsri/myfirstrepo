e2453bc5b866a49dbee6d683904a8b626d5ea14a
shashwatrajsri <ss4521811@gmail.com>
2021-10-05 19:50:36 +0530

    calculator

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const a = document.querySelectorAll('input')[0]
 const b = document.querySelectorAll('input')[1]
 const add = document.querySelector('#add')
+const sub = document.querySelector('#sub')
 const multiply = document.querySelector('#multiply')
-const resultbox = document.querySelector('.result')
+const result = document.querySelector('.result')
 
-const sum = () => {
+/*const sum = () => {
     const result = parseInt(a.value) + parseInt(b.value)  
     resultbox.innerHTML =result
 }
@@ -12,7 +13,7 @@ const sum = () => {
 const product = () => {
     const result = parseInt(a.value) * parseInt(b.value)  
     resultbox.innerHTML =result
-}
+}*/
 
 const calculate = (event,operation) => {
     //if(operation=='add'){
@@ -20,23 +21,24 @@ const calculate = (event,operation) => {
     //}else if(operation=='multiply'){
         //console.log('multiply')
     //}
+    console.log(event.target)
     switch(operation){
         case "add":
             resultbox.innerHTML= parseInt(a.value) + parseInt(b.value)
             break
         case "multiply":
-            resultbox.innerHTML= parseInt(a.value) * parseInt(b.value)
+            result.innerHTML= parseInt(a.value) * parseInt(b.value)
             break
-        case "subtract":
-            resultbox.innerHTML= parseInt(a.value) - parseInt(b.value)
+        case "sub":
+            result.innerHTML= parseInt(a.value) - parseInt(b.value)
             break
         default:
-            resultbox.innerHTML= "not a valid operation"
+            result.innerHTML= "not a valid operation"
     }
 }
 
 add.addEventListener('click', (event) => calculate(event,'add'))
 multiply.addEventListener('click', (event) => calculate(event,'multiply'))
-subtract.addEventListener('click', (event) => calculate(event,'subtract'))
+sub.addEventListener('click', (event) => calculate(event,'sub'))
 
 
