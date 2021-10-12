5ecc89ca5469272d6b4a640f2dcd15197e8d7879
shashwatrajsri <ss4521811@gmail.com>
2021-10-12 21:53:28 +0530

    NodeJS

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-const table = document.querySelector('table')
-console.log(table)
+const {add,subtract,multiply,divide}= require('./util')
 
-
-
-
-
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
-.then(data => {
-    data.forEach(user => {
-        let newRow = document.createElement('tr')
-        let dataID = document.createElement('td')
-        let dataIDtextNode = document.createTextNode('user.id')
-        dataID.appendChild(dataIDtextNode)
-        newRow.appendChild(dataID)
-
-        table.appendChild(newRow)
-    })
-})
+add()
+subtract()
+multiply()
+divide()
---
+++ util.js
@@ -0,0 +1,18 @@
+const add = () =>{
+    console.log('i add stuff')
+}
+const subtract = () =>{
+    console.log('i subtract stuff')
+}
+const multiply = () =>{
+    console.log('i multiply stuff')
+}
+const divide = () =>{
+    console.log('i divide stuff')
+}
+
+module.exports.add = add
+module.exports.subtract = subtract
+module.exports.multiply = multiply
+module.exports.divide = divide
+
