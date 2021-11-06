8f5bbd2bf7ff0e8108d4878c24bc809fdd86e357
shashwatrajsri <ss4521811@gmail.com>
2021-11-06 18:41:29 +0530

    JSx

--- REACT-DEMP/app.js
+++ REACT-DEMP/app.js
@@ -1,5 +1,27 @@
 const container = document.getElementById("app")
 
-const content = React.createElement('h1', {}, 'hi')
+const name = "Shashwat"
+
+const jsx = 
+<ul>
+    <li>I am a heading</li>
+    <li>I am a heading</li>
+    <li>I am a heading</li>
+    <ul>
+        <li>sdf</li>
+        <li>sdf</li>
+    </ul>
+</ul>
+
+const content = React.createElement('ul', {}, [
+    React.createElement('li', {}, "I am a heading"),
+    React.createElement('li', {}, "I am a heading"),
+    React.createElement('ul', {}, [
+        React.createElement('li', {}, "content"),
+        React.createElement('li', {}, "content"),
+        React.createElement('li', {}, "content"),
+        React.createElement('li', {}, "content"),
+    ]),
+])
 
 ReactDOM.render(content, container)
